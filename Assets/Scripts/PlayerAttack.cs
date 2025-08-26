255707d1282bcde2b8fbc829b2c15182fca6c9f7
Mabsas <139257914+Mabsas@users.noreply.github.com>
2025-08-27 03:03:01 +0600

    Attack FUnction & Attack Animation Added

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity; //so that player can attack from start
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && cooldownTimer > attackCooldown && playerMovement.canAttack()) //cooldowntimer ensure we a small break before next shot 
+            Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+    }
+
+}
