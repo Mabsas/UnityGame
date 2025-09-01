3caaffaee4ad560920a5848d51fa8b1999f9c6de
Mabsas <139257914+Mabsas@users.noreply.github.com>
2025-09-01 16:09:25 +0600

    Cameramovement following player along x & y axis

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour 
 {
+
+    //Room Camera
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float camersSpeed;
+    private float lookAhead;
+
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y,transform.position.z), ref velocity, speed);
+
+        //Room CAmera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y,transform.position.z), ref velocity, speed);
+        
+
+        //Follow Player 
+        transform.position = new Vector3(player.position.x + lookAhead, player.position.y,transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * camersSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
