fe54b552f4de277f5ec97d8581b48d8ffc067962
Mabsas <139257914+Mabsas@users.noreply.github.com>
2026-06-13 02:46:17 +0600

    Spike and Firetrap added

---
+++ Assets/Scripts/Enemies/Firetrap.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Firetrap : MonoBehaviour
+{
+
+    [SerializeField] private float damage;
+
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered; // when trap gets triggered
+    private bool active;  // when trap is active and can hurt the player 
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (!triggered)
+            {
+                StartCoroutine(ActivateFiretrap());
+            }
+            if(active) {
+
+                collision.GetComponent<Health>().TakeDamage(damage);
+            }
+        }
+    }
+
+    private IEnumerator ActivateFiretrap()
+    {
+        //turn the sprite red to notify the player and trigger the trap
+        triggered = true;
+        spriteRend.color = Color.red;  //turn the sprite red to notify the player
+            
+        //Wait for the delay , activate trap, turn on animation ,return color back to normal
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white;  //turn the sprite white back to normal
+        active = true;
+        anim.SetBool("activate", true);
+
+
+        //Wait for x seconds , deactivate trap and reset all variables and animator 
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activate", false);
+    }
+
+}
+
+
+
