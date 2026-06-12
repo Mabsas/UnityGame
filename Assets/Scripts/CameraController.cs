cfde5e52bf3b38f9a63375664ded9d0ac7460f7b
Mabsas <139257914+Mabsas@users.noreply.github.com>
2026-06-12 19:27:31 +0600

    player gets a temporary invisibility On collision

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -21,7 +21,7 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y,transform.position.z), ref velocity, speed);
         
 
-        ////Follow Player 
+        ///Follow Player 
         //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y,transform.position.z);
         //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * camersSpeed);
     }
--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,17 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
+
     public float currentHealth { get; private set; } // get ensure other file can access it  & private set ensure it can be updated only in this file
     private Animator anim;
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
 
 
     private void Awake()
     {
         currentHealth= startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -24,7 +34,7 @@ public class Health : MonoBehaviour
         {
             //player alive
             anim.SetTrigger("hurt");
-            
+            StartCoroutine(Invulnerability());
         }
 
         else
@@ -46,5 +56,22 @@ public class Health : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 
+    // On collision player gets a temporary invisibility
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);  // after first hit ignore collision btwn player and trap  to show the invisibilty effect so player doesnt lose helath during invisibility
 
-}
+        for(int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));  // Assigning time to each flas & multiply 2 beaces two flashes red and white 
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+
+        }
+
+        Physics2D.IgnoreLayerCollision(8, 9, false);  // activates collision again
+    }
+
+
+}   
