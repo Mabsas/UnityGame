38e9c690b9b6ad0b3a497b5b8cafcc9ec3e7bccc
Mabsas <139257914+Mabsas@users.noreply.github.com>
2026-06-16 03:11:40 +0600

    Enemy attacking player + animation added

--- Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -1,4 +1,3 @@
-using System.Threading;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
@@ -10,61 +9,65 @@ public class EnemyPatrol : MonoBehaviour
     [Header("Enemy")]
     [SerializeField] private Transform enemy;
 
-
-    [Header("Movement parameter")]
+    [Header("Movement parameters")]
     [SerializeField] private float speed;
     private Vector3 initScale;
     private bool movingLeft;
 
+    [Header("Idle Behaviour")]
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+
+    [Header("Enemy Animator")]
+    [SerializeField] private Animator anim;
 
     private void Awake()
     {
         initScale = enemy.localScale;
     }
+    private void OnDisable()
+    {
+        anim.SetBool("moving", false);
+    }
 
     private void Update()
     {
-
         if (movingLeft)
         {
-            if(enemy.position.x>=leftEdge.position.x)
+            if (enemy.position.x >= leftEdge.position.x)
                 MoveInDirection(-1);
             else
-            {
                 DirectionChange();
-            }
         }
         else
         {
-            if(enemy.position.x <= rightEdge.position.x)
+            if (enemy.position.x <= rightEdge.position.x)
                 MoveInDirection(1);
             else
-            {
                 DirectionChange();
-            }
         }
-        
     }
+
     private void DirectionChange()
     {
-        movingLeft = !movingLeft;
+        anim.SetBool("moving", false);
+        idleTimer += Time.deltaTime;
+
+        if (idleTimer > idleDuration)
+            movingLeft = !movingLeft;
     }
 
     private void MoveInDirection(int _direction)
     {
-        // Make enemy face direction
-        enemy.localScale = new Vector3(
-            Mathf.Abs(initScale.x) * _direction,
-            initScale.y,
-            initScale.z
-        );
+        idleTimer = 0;
+        anim.SetBool("moving", true);
 
-        // Move in that direction
-        enemy.position = new Vector3(
-            enemy.position.x + Time.deltaTime * _direction * speed,
-            enemy.position.y,
-            enemy.position.z
-        );
+        //Make enemy face direction
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction,
+            initScale.y, initScale.z);
+
+        //Move in that direction
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
+            enemy.position.y, enemy.position.z);
     }
-
 }
