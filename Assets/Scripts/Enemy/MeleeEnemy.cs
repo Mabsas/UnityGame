735e9cab79a3d228c7b413ffc350dc84b0dd86fa
Mabsas <139257914+Mabsas@users.noreply.github.com>
2026-06-15 17:29:05 +0600

    Setting up the enemy range of attack

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -1,16 +1,50 @@
+using System;
 using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+
+
+        //Attack only when player in sight 
+        if (PlayerInSight())
+        {
+            if(cooldownTimer >= attackCooldown)
+            {
+                //Attack
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool PlayerInSight()
     {
-        
+        RaycastHit2D hit = Physics2D.BoxCast(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y , boxCollider.bounds.size.z),
+            0,
+            Vector2.left,
+            0,
+            playerLayer);
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (boxCollider == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
+
