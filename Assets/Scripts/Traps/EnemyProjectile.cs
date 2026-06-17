c729691fae08ac652910e21a62a03ac53bbfd835
Mabsas <139257914+Mabsas@users.noreply.github.com>
2026-06-17 11:17:40 +0600

    Ranged Enemy attack with fireball

--- Assets/Scripts/Traps/EnemyProjectile.cs
+++ Assets/Scripts/Traps/EnemyProjectile.cs
@@ -1,20 +1,31 @@
-using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
-public class EnemyProjectile : EnemyDamage  // willdamage the player every time they touch
+public class EnemyProjectile : EnemyDamage
 {
     [SerializeField] private float speed;
     [SerializeField] private float resetTime;
     private float lifetime;
+    private Animator anim;
+    private BoxCollider2D coll;
+
+    private bool hit;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
+    }
 
     public void ActivateProjectile()
     {
+        hit = false;
         lifetime = 0;
         gameObject.SetActive(true);
+        coll.enabled = true;
     }
-
     private void Update()
     {
+        if (hit) return;
         float movementSpeed = speed * Time.deltaTime;
         transform.Translate(movementSpeed, 0, 0);
 
@@ -25,7 +36,17 @@ public class EnemyProjectile : EnemyDamage  // willdamage the player every time 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        base.OnTriggerEnter2D(collision);  // Execute logic from parent script first 
-        gameObject.SetActive(false); // when this hits any object deactivate arrow
+        hit = true;
+        base.OnTriggerEnter2D(collision); //Execute logic from parent script first
+        coll.enabled = false;
+
+        if (anim != null)
+            anim.SetTrigger("explode"); //When the object is a fireball explode it
+        else
+            gameObject.SetActive(false); //When this hits any object deactivate arrow
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
